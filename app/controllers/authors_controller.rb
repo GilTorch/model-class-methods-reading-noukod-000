02ce536c9eefe84415258277380f31a89954b6cf
Gilbert Torchon <gilbert@noukod.com>
2019-01-28 21:26:22 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,18 @@
 class AuthorsController < ApplicationController
+
   def show
     @author = Author.find(params[:id])
+    @authors = Author.all
+
+  if !params[:author].blank?
+    @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
+      end
+    else
+      @posts=Post.all
   end
 end
