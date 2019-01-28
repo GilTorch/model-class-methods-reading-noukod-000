1c6877a3c1dc6742432646292f5efcc450cd699a
Gilbert Torchon <gilbert@noukod.com>
2019-01-28 21:19:24 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,5 +3,15 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
     @authors = Author.all
+
+  if !params[:author].blank?
+    @posts = Post.where(author: params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+      else
+        @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+      end
+    else
   end
 end
